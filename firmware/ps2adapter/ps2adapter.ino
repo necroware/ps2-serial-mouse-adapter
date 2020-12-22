a1f27e95e95027fb22afd20b89f79b6496b2eeaf
Filipp Andjelo <filipp@andjelo.de>
2020-12-22 01:00:28 +0100

    Reset on interrupt

--- firmware/ps2adapter/ps2adapter.ino
+++ firmware/ps2adapter/ps2adapter.ino
@@ -4,17 +4,15 @@ static const int PS2_CLOCK = 2;
 static const int PS2_DATA  = 17;
 static const int RS232_RTS = 3;
 static const int RS232_TX  = 4;
+static const int LED = 13;
 
 static Ps2Mouse mouse(PS2_CLOCK, PS2_DATA, Ps2Mouse::Mode::remote);
-static bool reset = false;
 
 static void sendSerialBit(int data) {
   // Delay between the signals to match 1200 baud
   static const auto usDelay = 1000000 / 1200;
-  if (!reset) {
-      digitalWrite(RS232_TX, data);
-      delayMicroseconds(usDelay);
-  }
+  digitalWrite(RS232_TX, data);
+  delayMicroseconds(usDelay);
 }
 
 static void sendSerialByte(byte data) {
@@ -41,51 +39,44 @@ static void sendToSerial(const Ps2Mouse::Data& data) {
   byte rb = data.rightButton << 4;
   byte dx = static_cast<byte>(constrain(data.xMovement, -127, 127));
   byte dy = static_cast<byte>(constrain(-data.yMovement, -127, 127));
+  byte mb = data.middleButton ? 0x20 : 0x00;
   sendSerialByte(0x40 | lb | rb | ((dy >> 4) & 0xC) | (dx >> 6));
   sendSerialByte(dx & 0x3F);
   sendSerialByte(dy & 0x3F);
-  byte mb = data.middleButton ? 0x20 : 0x00;
   sendSerialByte(mb);
 }
 
-static void initPs2Mouse() {
-  Serial.print("Activating PS/2 mouse... ");
-  if (!mouse.reset()) {
-    Serial.println("failed");
-  } else {
-    Serial.println("ok");
-  }
-}
-
-static void initSerialMouse() {
-  Serial.print("Activating Serial Port... ");
-  pinMode(RS232_TX, OUTPUT);
-  reset = false;
-  sendSerialByte('M');
-  sendSerialByte('3');
-  Serial.println("ok");
+static void listenRTS() {
+  auto handler = []() {
+    digitalWrite(LED, HIGH);
+    digitalWrite(RS232_TX, HIGH);
+    delayMicroseconds(10000);
+    sendSerialByte('M');
+    sendSerialByte('3');
+    delayMicroseconds(10000);
+    digitalWrite(LED, LOW);
+    void (*resetHack)() = 0;
+    resetHack();
+  };
+  attachInterrupt(digitalPinToInterrupt(RS232_RTS), handler, FALLING);
 }
 
 void setup() {
   // PS/2 Data input must be initialized shortly after power on,
   // or the mouse will not initialize
   pinMode(PS2_DATA, INPUT_PULLUP);
-  pinMode(13, OUTPUT);
+  pinMode(RS232_TX, OUTPUT);
+  digitalWrite(RS232_TX, HIGH);
+  pinMode(LED, OUTPUT);
   Serial.begin(9600);
-  initPs2Mouse();
-  auto handler = []() { reset = true; digitalWrite(RS232_TX, HIGH); };
-  attachInterrupt(digitalPinToInterrupt(RS232_RTS), handler, FALLING);
+  Serial.println("Starting...");
+  listenRTS();
+  mouse.reset();
 }
 
 void loop() {
-  if (reset) {
-      digitalWrite(13, HIGH);
-      initSerialMouse();
-      digitalWrite(13, LOW);
-  } else {
-    Ps2Mouse::Data data;
-    if (mouse.readData(data)) {
-      sendToSerial(data);
-    }
+  Ps2Mouse::Data data;
+  if (mouse.readData(data)) {
+    sendToSerial(data);
   }
 }
