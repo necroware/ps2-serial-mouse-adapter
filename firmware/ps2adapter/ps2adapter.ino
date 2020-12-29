0a1d2e2ea859a987ad9cd3ad043eed7bbcef02eb
Filipp Andjelo <filipp@andjelo.de>
2020-12-29 17:48:59 +0100

    Implement 2-/3-button mode switch

--- firmware/ps2adapter/ps2adapter.ino
+++ firmware/ps2adapter/ps2adapter.ino
@@ -4,9 +4,12 @@ static const int PS2_CLOCK = 2;
 static const int PS2_DATA  = 17;
 static const int RS232_RTS = 3;
 static const int RS232_TX  = 4;
+static const int JP12 = 11;
+static const int JP34 = 12;
 static const int LED = 13;
 
 static Ps2Mouse mouse(PS2_CLOCK, PS2_DATA);
+static bool threeButtons = false;
 
 static void sendSerialBit(int data) {
   // Delay between the signals to match 1200 baud
@@ -39,26 +42,35 @@ static void sendToSerial(const Ps2Mouse::Data& data) {
   auto dy = constrain(-data.yMovement, -127, 127);
   byte lb = data.leftButton ? 0x20 : 0;
   byte rb = data.rightButton ? 0x10 : 0;
-  byte mb = data.middleButton ? 0x20 : 0;
   sendSerialByte(0x40 | lb | rb | ((dy >> 4) & 0xC) | ((dx >> 6) & 0x3));
   sendSerialByte(dx & 0x3F);
   sendSerialByte(dy & 0x3F);
-  sendSerialByte(mb);
+  if (threeButtons) {
+    byte mb = data.middleButton ? 0x20 : 0;
+    sendSerialByte(mb);
+  }
 }
 
 static void initSerialPort() {
   Serial.println("Starting serial port");
-  digitalWrite(LED, HIGH);
   digitalWrite(RS232_TX, HIGH);
   delayMicroseconds(10000);
   sendSerialByte('M');
-  sendSerialByte('3');
+  if(threeButtons) {
+    sendSerialByte('3');
+    Serial.println("Init 3-buttons mode");
+  }
   delayMicroseconds(10000);
-  digitalWrite(LED, LOW);
 
   Serial.println("Listening on RTS");
   void (*resetHack)() = 0;
   attachInterrupt(digitalPinToInterrupt(RS232_RTS), resetHack, FALLING);
+}
+
+static void initPs2Port() {
+  Serial.println("Reseting PS/2 mouse");
+  mouse.reset();
+  mouse.setSampleRate(20);
 }
 
 void setup() {
@@ -66,13 +78,15 @@ void setup() {
   // or the mouse will not initialize
   pinMode(PS2_DATA, INPUT_PULLUP);
   pinMode(RS232_TX, OUTPUT);
+  pinMode(JP12, INPUT_PULLUP);
+  pinMode(JP34, INPUT_PULLUP);
   pinMode(LED, OUTPUT);
-
+  digitalWrite(LED, HIGH);
+  threeButtons = digitalRead(JP12);
   Serial.begin(115200);
   initSerialPort();
-  Serial.println("Reseting PS/2 mouse");
-  mouse.reset();
   Serial.println("Setup done!");
+  digitalWrite(LED, LOW);
 }
 
 void loop() {
